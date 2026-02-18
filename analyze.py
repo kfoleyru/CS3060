6438bb5e1c21ba88a6b0393defbda03d3825bbae
kfoleyru <kfoleyru@uvm.edu>
2026-02-17 19:47:24 -0500

    Adjusted amplitude, frequency, and phaseOffset values from front and back leg to make the robot run away from its startibg place (completed assign 5)

--- analyze.py
+++ analyze.py
@@ -1,11 +1,18 @@
 import matplotlib.pyplot as pyplot
 import numpy
 
-backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
-frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
+# backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
+# frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
 
-pyplot.plot(backLegSensorValues, label="Back Leg Sensor Values")
-pyplot.plot(frontLegSensorValues, label="Front Leg Sensor Values")
+# pyplot.plot(backLegSensorValues, label="Back Leg Sensor Values", linewidth=0.5)
+# pyplot.plot(frontLegSensorValues, label="Front Leg Sensor Values", linewidth=0.5)
+
+targetAngleValuesB = numpy.load('data/targetAngleValuesBack.npy')
+targetAngleValuesF = numpy.load('data/targetAngleValuesFront.npy')
+
+
+pyplot.plot(targetAngleValuesB, label="Back Leg Target Angle Values", linewidth=1)
+pyplot.plot(targetAngleValuesF, label="Front Leg Target Angle Values", linewidth=1)
 
 pyplot.legend(loc="upper right")
 
--- simulate.py
+++ simulate.py
@@ -1,10 +1,12 @@
-import math
 import numpy
 import pybullet as p
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import random
 import time
+
+STEPS = 5000
+PI = numpy.pi
 
 
 physicsClient = p.connect(p.GUI)
@@ -19,10 +21,25 @@ p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
 
-backLegSensorValues = numpy.zeros(1000)
-frontLegSensorValues = numpy.zeros(1000)
+amplitudeB = PI/4
+frequencyB = 30
+phaseOffsetB = 0
 
-for i in range(1000):
+amplitudeF = PI/4
+frequencyF = 15
+phaseOffsetF = PI/3
+
+backLegSensorValues = numpy.zeros(STEPS)
+frontLegSensorValues = numpy.zeros(STEPS)
+targetAnglesB = amplitudeB * numpy.sin(frequencyB * numpy.linspace(0, 2*PI, STEPS) + phaseOffsetB)
+targetAnglesF = amplitudeF * numpy.sin(frequencyF * numpy.linspace(0, 2*PI, STEPS) + phaseOffsetF)
+
+# numpy.save('data/targetAngleValuesBack.npy', targetAnglesB)
+# numpy.save('data/targetAngleValuesFront.npy', targetAnglesF)
+
+# # exit()
+
+for i in range(STEPS):
 	p.stepSimulation()
 	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
@@ -30,15 +47,15 @@ for i in range(1000):
 		bodyIndex = robotId, 
 		jointName = b'Torso_BackLeg',
 		controlMode = p.POSITION_CONTROL,
-		targetPosition = random.random()*random.uniform(-math.pi/2.0,math.pi/2.0),
+		targetPosition = targetAnglesB[i],
 		maxForce = 25)
 	pyrosim.Set_Motor_For_Joint(
 		bodyIndex = robotId, 
 		jointName = b'Torso_FrontLeg',
 		controlMode = p.POSITION_CONTROL,
-		targetPosition = random.random()*random.uniform(-math.pi/2.0,math.pi/2.0),
+		targetPosition = targetAnglesF[i],
 		maxForce = 25)
-	time.sleep(0.01)
+	time.sleep(0.001)
 
 numpy.save('data/backLegSensorValues.npy', backLegSensorValues)
 numpy.save('data/frontLegSensorValues.npy', frontLegSensorValues)
