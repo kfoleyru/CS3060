e829d58d9e708c77745de2da78cc46cec22f7370
kfoleyru <kfoleyru@uvm.edu>
2026-02-23 21:27:06 -0500

    Added constants file and adjusted simulate.py constants to pull from that

--- constants.py
+++ constants.py
@@ -1,12 +1,20 @@
+"""
+Author: Kalei Foley-Rutherfurd
+Description: Constants for use in simulate.py
+"""
 import numpy
 
-steps = 5000
 pi = numpy.pi
 
+# Number of steps in simulation
+steps = 5000
+
+# Angle values for motors – Back Leg
 amplitudeB = pi/4
 frequencyB = 30
 phaseOffsetB = 0
 
+# Angle values for motors – Front Leg
 amplitudeF = pi/4
 frequencyF = 15
 phaseOffsetF = pi/3
--- simulate.py
+++ simulate.py
@@ -1,66 +1,54 @@
-# import constants as c
-# import numpy
-# import pybullet as p
-# import pybullet_data
-# import pyrosim.pyrosim as pyrosim
-# import random
-# import time
+import constants as c
+import numpy
+import pybullet as p
+import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import random
+import time
 
-# steps = 5000
-# pi = numpy.pi
+pi = numpy.pi
 
+physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
-# physicsClient = p.connect(p.GUI)
-# p.setAdditionalSearchPath(pybullet_data.getDataPath())
-# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+p.setGravity(0,0,-9.8,physicsClient)
+planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 
-# p.setGravity(0,0,-9.8,physicsClient)
-# planeId = p.loadURDF("plane.urdf")
-# robotId = p.loadURDF("body.urdf")
+p.loadSDF("world.sdf")
 
-# p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
 
-# pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorValues = numpy.zeros(c.steps)
+frontLegSensorValues = numpy.zeros(c.steps)
+targetAnglesB = c.amplitudeB * numpy.sin(c.frequencyB * numpy.linspace(0, 2*pi, c.steps) + c.phaseOffsetB)
+targetAnglesF = c.amplitudeF * numpy.sin(c.frequencyF * numpy.linspace(0, 2*pi, c.steps) + c.phaseOffsetF)
 
-# amplitudeB = c.amplitudeB
-# frequencyB = c.frequencyB
-# phaseOffsetB = c.phaseOffsetB
+# numpy.save('data/targetAngleValuesBack.npy', targetAnglesB)
+# numpy.save('data/targetAngleValuesFront.npy', targetAnglesF)
 
-# amplitudeF = c.amplitudeF
-# frequencyF = c.frequencyF
-# phaseOffsetF = c.phaseOffsetF
+# # exit()
 
-# backLegSensorValues = numpy.zeros(steps)
-# frontLegSensorValues = numpy.zeros(steps)
-# targetAnglesB = amplitudeB * numpy.sin(frequencyB * numpy.linspace(0, 2*pi, steps) + phaseOffsetB)
-# targetAnglesF = amplitudeF * numpy.sin(frequencyF * numpy.linspace(0, 2*pi, steps) + phaseOffsetF)
+for i in range(c.steps):
+	p.stepSimulation()
+	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+	pyrosim.Set_Motor_For_Joint(
+		bodyIndex = robotId, 
+		jointName = b'Torso_BackLeg',
+		controlMode = p.POSITION_CONTROL,
+		targetPosition = targetAnglesB[i],
+		maxForce = 25)
+	pyrosim.Set_Motor_For_Joint(
+		bodyIndex = robotId, 
+		jointName = b'Torso_FrontLeg',
+		controlMode = p.POSITION_CONTROL,
+		targetPosition = targetAnglesF[i],
+		maxForce = 25)
+	time.sleep(0.001)
 
-# # numpy.save('data/targetAngleValuesBack.npy', targetAnglesB)
-# # numpy.save('data/targetAngleValuesFront.npy', targetAnglesF)
+numpy.save('data/backLegSensorValues.npy', backLegSensorValues)
+numpy.save('data/frontLegSensorValues.npy', frontLegSensorValues)
 
-# # # exit()
-
-# for i in range(steps):
-# 	p.stepSimulation()
-# 	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-# 	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-# 	pyrosim.Set_Motor_For_Joint(
-# 		bodyIndex = robotId, 
-# 		jointName = b'Torso_BackLeg',
-# 		controlMode = p.POSITION_CONTROL,
-# 		targetPosition = targetAnglesB[i],
-# 		maxForce = 25)
-# 	pyrosim.Set_Motor_For_Joint(
-# 		bodyIndex = robotId, 
-# 		jointName = b'Torso_FrontLeg',
-# 		controlMode = p.POSITION_CONTROL,
-# 		targetPosition = targetAnglesF[i],
-# 		maxForce = 25)
-# 	time.sleep(0.001)
-
-# numpy.save('data/backLegSensorValues.npy', backLegSensorValues)
-# numpy.save('data/frontLegSensorValues.npy', frontLegSensorValues)
-
-# p.disconnect()
-
-pass
+p.disconnect()
