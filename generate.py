57deac5e9f4b309ba405ac0cb36795d2fd5350cf
kfoleyru <kfoleyru@uvm.edu>
2026-03-25 18:29:10 -0400

    completed step 54

--- generate.py
+++ generate.py
@@ -9,19 +9,6 @@ def create_world():
 	pyrosim.Start_SDF("world.sdf")
 	pyrosim.End()
 
-# def create_robot():
-# 	pyrosim.Start_URDF("body.urdf")
-# 	pyrosim.Send_Cube(name = "Torso", pos=[0,0,1.5], size=[LENGTH,WIDTH,HEIGHT])
-# 	pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , 
-# 							type = "revolute", position = [0.5,0,1])
-# 	pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , 
-# 							type = "revolute", position = [-0.5,0,1])
-# 	pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5], size=[LENGTH,WIDTH,HEIGHT])
-# 	pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5], size=[LENGTH,WIDTH,HEIGHT])
-
-
-# 	pyrosim.End()
-
 def generate_body():
 	pyrosim.Start_URDF("body.urdf")
 	pyrosim.Send_Cube(name = "Torso", pos=[0,0,1.5], size=[LENGTH,WIDTH,HEIGHT])
@@ -38,11 +25,12 @@ def generate_body():
 def generate_brain():
 	pyrosim.Start_NeuralNetwork("brain.nndf")
 	pyrosim.Send_Sensor_Neuron(name = 0, linkName = "Torso")
+	pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
+	pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
 	pyrosim.End()
 
 
 if __name__ == "__main__":
 	create_world()
-	# create_robot()
 	generate_body()
 	generate_brain()
--- pyrosim/neuralNetwork.py
+++ pyrosim/neuralNetwork.py
@@ -32,6 +32,8 @@ class NEURAL_NETWORK:
         for name in self.neurons:
             if self.neurons[name].Is_Sensor_Neuron():
                 self.neurons[name].Update_Sensor_Neuron()
+            else:
+                self.neurons[name].Update_Hidden_Or_Motor_Neuron()
 
 
 
