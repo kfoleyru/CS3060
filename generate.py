0cb3068f6c758ddc68505f2cc64a2d588f3df29a
kfoleyru <kfoleyru@uvm.edu>
2026-04-07 11:14:21 -0400

    Completed step 33

--- generate.py
+++ generate.py
@@ -25,10 +25,14 @@ def generate_body():
 def generate_brain():
 	pyrosim.Start_NeuralNetwork("brain.nndf")
 	pyrosim.Send_Sensor_Neuron(name = 0, linkName = "Torso")
+	pyrosim.Send_Sensor_Neuron(name = 1, linkName = "FrontLeg")
+	pyrosim.Send_Sensor_Neuron(name = 2, linkName = "BackLeg")
+	
 	pyrosim.Send_Motor_Neuron(name = 3 , jointName = "Torso_BackLeg")
 	pyrosim.Send_Motor_Neuron(name = 4 , jointName = "Torso_FrontLeg")
 	# connect neuron 1 to neuron 3
 	pyrosim.Send_Synapse(sourceNeuronName = 1, targetNeuronName = 3, weight = 1.0)
+	
 	pyrosim.End()
 
 
--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -70,17 +70,17 @@ class NEURON:
         self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name()))
 
     def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
-        self.Set_Value(0)
+        self.Set_Value(0.0)
 
         print(self.value)
-        
         for key in synapses.keys():
             if (key[1] == self.Get_Name()):
                 self.Allow_Presunaptic_Neuron_To_Influence_Me(synapses[key].Get_Weight(), neurons[key[0]].Get_Value())
-        
+        print(self.value)
+        exit()
     def Allow_Presunaptic_Neuron_To_Influence_Me(self, weight, pnval):
-        pass
-        
+        self.Add_To_Value(weight * pnval)
+
 
 # -------------------------- Private methods -------------------------
 
